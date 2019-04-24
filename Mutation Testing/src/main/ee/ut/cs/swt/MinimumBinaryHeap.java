9401f3c7b8daac7a0f0270864c833f5dfb715269
Gregor Eesmaa <gregor.eesmaa@ut.ee>
2019-04-24 20:46:01 +0300

    Better tests. Finalizing.

--- Mutation Testing/src/main/ee/ut/cs/swt/MinimumBinaryHeap.java
+++ Mutation Testing/src/main/ee/ut/cs/swt/MinimumBinaryHeap.java
@@ -5,121 +5,122 @@ import java.util.List;
 
 public class MinimumBinaryHeap {
 
-	private List<Integer> heap = new ArrayList<>();
+    private List<Integer> heap = new ArrayList<>();
 
-	/**
-	 * Switches elements in the array such that heap property holds for every element.
-	 *
-	 * @param array
-	 */
-	public void minHeapify(List<Integer> array) {
-		heap = new ArrayList<>(array);
-		for (int i = 0; i < array.size(); i++) {
-			bubbleUp(i);
-		}
-		array = heap;
-	}
+    /**
+     * Switches elements in the array such that heap property holds for every element.
+     *
+     * @param array
+     */
+    public void minHeapify(List<Integer> array) {
+        heap = new ArrayList<>(array);
+        for (int i = 0; i < array.size(); i++) {
+            bubbleUp(i);
+        }
+        array = heap;
+    }
 
-	/**
-	 * Removes and returns element with minimum value. On empty
-	 * heap throws IllegalArgumentException.
-	 *
-	 * @return Element at the top of the heap.
-	 */
-	public int exractMin() {
-		try {
-			int min = heap.get(0);
-			int leaf = heap.get(heap.size() - 1);
-			heap.set(0, leaf);
-			heap.remove(heap.size() - 1);
-			bubbleDown(0);
-			return min;
-		} catch (IndexOutOfBoundsException e) {
-			throw new IllegalArgumentException();
-		}
-	}
+    /**
+     * Removes and returns element with minimum value. On empty
+     * heap throws IllegalArgumentException.
+     *
+     * @return Element at the top of the heap.
+     */
+    public int extractMin() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        int min = heap.get(0);
+        int leaf = heap.get(heap.size() - 1);
+        heap.set(0, leaf);
+        heap.remove(heap.size() - 1);
+        bubbleDown(0);
+        return min;
+    }
 
-	/**
-	 * Swaps two elements in the heap
-	 */
-	public void swap(int a, int b) {
-		int value = heap.get(a);
-		heap.set(a, heap.get(b));
-		heap.set(b, value);
-	}
+    /**
+     * Swaps two elements in the heap
+     */
+    public void swap(int a, int b) {
+        int value = heap.get(a);
+        heap.set(a, heap.get(b));
+        heap.set(b, value);
+    }
 
 
-	/**
-	 * Adds element to the heap.
-	 *
-	 * @param element
-	 */
-	public void add(int element) {
-		heap.add(element);
-		bubbleUp(heap.size() - 1);
-	}
+    /**
+     * Adds element to the heap.
+     *
+     * @param element
+     */
+    public void add(int element) {
+        heap.add(element);
+        bubbleUp(heap.size() - 1);
+    }
 
-	public void bubbleUp(int pos) {
-		if (pos == 0) return;
-		int parent = (pos) / 2;
-		while (heap.get(pos) < heap.get(parent)) {
-			swap(pos, parent);
-			pos = parent;
-			parent = (pos - 1) / 2;
-		}
-	}
+    public void bubbleUp(int pos) {
+        if (pos == 0) { return; }
+        int parent = (pos - 1) / 2;
+        while (heap.get(pos) < heap.get(parent)) {
+            swap(pos, parent);
+            pos = parent;
+            parent = (pos - 1) / 2;
+        }
+    }
 
-	public void bubbleDown(int pos) {
-		int leftChild = 2 * pos + 1;
-		int rightChild = 2 * pos + 2;
-		int min = pos;
-		int maxSize = heap.size();
+    public void bubbleDown(int pos) {
+        int leftChild = 2 * pos + 1;
+        int rightChild = 2 * pos + 2;
+        int min = pos;
+        int maxSize = heap.size();
 
-		// when the left child is smaller
-		if (leftChild < maxSize && heap.get(leftChild) < heap.get(min)) {
-			min = leftChild;
-		}
-		// when the right child is smaller
-		if (rightChild < maxSize && heap.get(rightChild) < heap.get(min)) {
-			min = rightChild;
-		}
-		if (min != pos) {
-			swap(min, pos);
-			bubbleDown(min);
-		}
-	}
+        // when the left child is smaller
+        if (leftChild < maxSize
+            && heap.get(leftChild) < heap.get(min)) {
+            min = leftChild;
+        }
+        // when the right child is smaller
+        if (rightChild < maxSize
+            && heap.get(rightChild) < heap.get(min)) {
+            min = rightChild;
+        }
+        if (min != pos) {
+            swap(min, pos);
+            bubbleDown(min);
+        }
+    }
 
-	/**
-	 * Removes element from the heap if possible.
-	 *
-	 * @param element
-	 * @return True if element was removed and false otherwise.
-	 */
-	public boolean remove(int element) {
-		int index = heap.indexOf(element);
-		if (index == -1) {
-			return false;
-		}
+    /**
+     * Removes element from the heap if possible.
+     *
+     * @param element
+     * @return True if element was removed and false otherwise.
+     */
+    public boolean remove(int element) {
+        int index = heap.indexOf(element);
+        if (index == -1) {
+            return false;
+        }
 
-		heap.set(index, heap.get(heap.size() - 1));
-		heap.remove(heap.size() - 1);
-		bubbleDown(index);
-		return true;
-	}
+        heap.set(index, heap.get(heap.size() - 1));
+        heap.remove(heap.size() - 1);
+        bubbleDown(index);
+        return true;
+    }
 
-	/**
-	 * @return True if heap is empty and false otherwise.
-	 */
-	public boolean isEmpty() {
-		return heap.isEmpty();
-	}
+    /**
+     * @return True if heap is empty and false otherwise.
+     */
+    public boolean isEmpty() {
+        return heap.isEmpty();
+    }
 
-	/**
-	 * @return List containing the heap.
-	 * (for testing purposes)
-	 */
-	public List<Integer> getArray() {
-		return heap;
-	}
+    /**
+     * @return List containing the heap.
+     * (for testing purposes)
+     */
+    public List<Integer> getArray() {
+        return heap;
+    }
 
 }
--- Mutation Testing/src/test/ee/ut/cs/swt/MinimumBinaryHeapTest.java
+++ Mutation Testing/src/test/ee/ut/cs/swt/MinimumBinaryHeapTest.java
@@ -13,162 +13,184 @@ import static org.junit.Assert.*;
 public class MinimumBinaryHeapTest {
 
     private MinimumBinaryHeap heap;
-    private List<Integer> result;
 
     @Before
     public void setUp() {
         heap = new MinimumBinaryHeap();
-        result = new ArrayList<>();
-        result.add(10);
-        result.add(5);
-        result.add(4);
-        result.add(8);
-        result.add(12);
     }
 
     @Test
     public void minHeapifyTest() {
-        heap.minHeapify(result);
-        assertEquals(4, heap.getArray().get(0), 0);
-        assertEquals(5, heap.getArray().get(1), 0);
-        assertEquals(10, heap.getArray().get(2), 0);
-
-    }
-
-    @Test
-    public void swapTest() {
-        heap.add(5);
-        heap.add(8);
-        heap.add(13);
-        heap.swap(1, 2);
-        assertEquals(5, heap.getArray().get(0), 0);
-        assertEquals(13, heap.getArray().get(1), 0);
-        assertEquals(8, heap.getArray().get(2), 0);
+        heap.minHeapify(Arrays.asList(5, 17, 15, 9, 19, 11, 16, 8, 12, 10, 2, 7, 3, 14, 13, 20, 6, 1, 4, 18));
+        assertEquals(Arrays.asList(1, 2, 3, 4, 8, 7, 13, 9, 5, 18, 10, 15, 11, 16, 14, 20, 17, 12, 6, 19),
+                     heap.getArray());
     }
 
     @Test
     public void addTest() {
         heap.add(10);
+        assertEquals(Collections.singletonList(10), heap.getArray());
+
         heap.add(5);
+        assertEquals(Arrays.asList(5, 10), heap.getArray());
+
         heap.add(4);
+        assertEquals(Arrays.asList(4, 10, 5), heap.getArray());
+
         heap.add(8);
+        assertEquals(Arrays.asList(4, 8, 5, 10), heap.getArray());
+
         heap.add(12);
-        assertEquals(4, heap.getArray().get(0), 0);
-        assertEquals(5, heap.getArray().get(1), 0);
-        assertEquals(10, heap.getArray().get(2), 0);
+        assertEquals(Arrays.asList(4, 8, 5, 10, 12), heap.getArray());
+    }
+
+    @Test
+    public void removeWithEmptyTest() {
+        boolean b = heap.remove(9);
+        assertFalse(b);
+        assertTrue(heap.isEmpty());
+    }
+
+
+    @Test
+    public void removeNonexistentTest() {
+        heap.getArray().addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
+        List<Integer> startState = new ArrayList<>(heap.getArray());
+
+        boolean b = heap.remove(9);
+
+        assertFalse(b);
+        assertEquals(startState, heap.getArray());
     }
 
     @Test
     public void removeTest() {
-        heap.minHeapify(Arrays.asList(1,2,3,4));
+        heap.getArray().addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
 
-        boolean b = heap.remove(3);
+        boolean b = heap.remove(4);
         assertTrue(b);
-        assertEquals(Arrays.asList(1,2,4), heap.getArray());
+        assertEquals(Arrays.asList(1, 2, 3, 8, 5, 6, 7), heap.getArray());
+
+        b = heap.remove(8);
+        assertTrue(b);
+        assertEquals(Arrays.asList(1, 2, 3, 7, 5, 6), heap.getArray());
+
+        b = heap.remove(1);
+        assertTrue(b);
+        assertEquals(Arrays.asList(2, 5, 3, 7, 6), heap.getArray());
+
+        b = heap.remove(3);
+        assertTrue(b);
+        assertEquals(Arrays.asList(2, 5, 6, 7), heap.getArray());
+
+        b = heap.remove(5);
+        assertTrue(b);
+        assertEquals(Arrays.asList(2, 7, 6), heap.getArray());
 
         b = heap.remove(2);
         assertTrue(b);
-        assertEquals(Arrays.asList(1,4), heap.getArray());
+        assertEquals(Arrays.asList(6, 7), heap.getArray());
 
-        b = heap.remove(4);
+        b = heap.remove(6);
         assertTrue(b);
-        assertEquals(Collections.singletonList(1), heap.getArray());
+        assertEquals(Collections.singletonList(7), heap.getArray());
 
-        b = heap.remove(1);
+        b = heap.remove(7);
         assertTrue(b);
         assertEquals(Collections.emptyList(), heap.getArray());
     }
 
     @Test
     public void extractMinTest() {
-        heap.minHeapify(Arrays.asList(5, 17, 15, 9, 19, 11, 16, 8, 12, 10, 2, 7, 3, 14, 13, 20, 6, 1, 4, 18));
+        heap.getArray().addAll(Arrays.asList(1, 2, 3, 4, 8, 7, 13, 9, 5, 18, 10, 15, 11, 16, 14, 20, 17, 12, 6, 19));
 
-        int a = heap.exractMin();
+        int a = heap.extractMin();
         assertEquals(1, a);
-        assertEquals(Arrays.asList(2, 4, 3, 6, 10, 7, 5, 13, 8, 18, 17, 11, 16, 14, 15, 20, 19, 9, 12),
+        assertEquals(Arrays.asList(2, 4, 3, 5, 8, 7, 13, 9, 6, 18, 10, 15, 11, 16, 14, 20, 17, 12, 19),
                      heap.getArray());
 
-        a = heap.exractMin();
+        a = heap.extractMin();
         assertEquals(2, a);
-        assertEquals(Arrays.asList(3, 4, 5, 6, 10, 7, 12, 13, 8, 18, 17, 11, 16, 14, 15, 20, 19, 9), heap.getArray());
+        assertEquals(Arrays.asList(3, 4, 7, 5, 8, 11, 13, 9, 6, 18, 10, 15, 19, 16, 14, 20, 17, 12), heap.getArray());
 
-        a = heap.exractMin();
+        a = heap.extractMin();
         assertEquals(3, a);
-        assertEquals(Arrays.asList(4, 6, 5, 8, 10, 7, 12, 13, 9, 18, 17, 11, 16, 14, 15, 20, 19), heap.getArray());
+        assertEquals(Arrays.asList(4, 5, 7, 6, 8, 11, 13, 9, 12, 18, 10, 15, 19, 16, 14, 20, 17), heap.getArray());
 
-        a = heap.exractMin();
+        a = heap.extractMin();
         assertEquals(4, a);
-        assertEquals(Arrays.asList(5, 6, 7, 8, 10, 11, 12, 13, 9, 18, 17, 19, 16, 14, 15, 20), heap.getArray());
+        assertEquals(Arrays.asList(5, 6, 7, 9, 8, 11, 13, 17, 12, 18, 10, 15, 19, 16, 14, 20), heap.getArray());
 
-        a = heap.exractMin();
+        a = heap.extractMin();
         assertEquals(5, a);
-        assertEquals(Arrays.asList(6, 8, 7, 9, 10, 11, 12, 13, 20, 18, 17, 19, 16, 14, 15), heap.getArray());
+        assertEquals(Arrays.asList(6, 8, 7, 9, 10, 11, 13, 17, 12, 18, 20, 15, 19, 16, 14), heap.getArray());
 
-        a = heap.exractMin();
+        a = heap.extractMin();
         assertEquals(6, a);
-        assertEquals(Arrays.asList(7, 8, 11, 9, 10, 15, 12, 13, 20, 18, 17, 19, 16, 14), heap.getArray());
+        assertEquals(Arrays.asList(7, 8, 11, 9, 10, 14, 13, 17, 12, 18, 20, 15, 19, 16), heap.getArray());
 
-        a = heap.exractMin();
+        a = heap.extractMin();
         assertEquals(7, a);
-        assertEquals(Arrays.asList(8, 9, 11, 13, 10, 15, 12, 14, 20, 18, 17, 19, 16), heap.getArray());
+        assertEquals(Arrays.asList(8, 9, 11, 12, 10, 14, 13, 17, 16, 18, 20, 15, 19), heap.getArray());
 
-        a = heap.exractMin();
+        a = heap.extractMin();
         assertEquals(8, a);
-        assertEquals(Arrays.asList(9, 10, 11, 13, 16, 15, 12, 14, 20, 18, 17, 19), heap.getArray());
+        assertEquals(Arrays.asList(9, 10, 11, 12, 18, 14, 13, 17, 16, 19, 20, 15), heap.getArray());
 
-        a = heap.exractMin();
+        a = heap.extractMin();
         assertEquals(9, a);
-        assertEquals(Arrays.asList(10, 13, 11, 14, 16, 15, 12, 19, 20, 18, 17), heap.getArray());
+        assertEquals(Arrays.asList(10, 12, 11, 15, 18, 14, 13, 17, 16, 19, 20), heap.getArray());
 
-        a = heap.exractMin();
+        a = heap.extractMin();
         assertEquals(10, a);
-        assertEquals(Arrays.asList(11, 13, 12, 14, 16, 15, 17, 19, 20, 18), heap.getArray());
+        assertEquals(Arrays.asList(11, 12, 13, 15, 18, 14, 20, 17, 16, 19), heap.getArray());
 
-        a = heap.exractMin();
+        a = heap.extractMin();
         assertEquals(11, a);
-        assertEquals(Arrays.asList(12, 13, 15, 14, 16, 18, 17, 19, 20), heap.getArray());
+        assertEquals(Arrays.asList(12, 15, 13, 16, 18, 14, 20, 17, 19), heap.getArray());
 
-        a = heap.exractMin();
+        a = heap.extractMin();
         assertEquals(12, a);
-        assertEquals(Arrays.asList(13, 14, 15, 19, 16, 18, 17, 20), heap.getArray());
+        assertEquals(Arrays.asList(13, 15, 14, 16, 18, 19, 20, 17), heap.getArray());
 
-        a = heap.exractMin();
+        a = heap.extractMin();
         assertEquals(13, a);
-        assertEquals(Arrays.asList(14, 16, 15, 19, 20, 18, 17), heap.getArray());
+        assertEquals(Arrays.asList(14, 15, 17, 16, 18, 19, 20), heap.getArray());
 
-        a = heap.exractMin();
+        a = heap.extractMin();
         assertEquals(14, a);
-        assertEquals(Arrays.asList(15, 16, 17, 19, 20, 18), heap.getArray());
+        assertEquals(Arrays.asList(15, 16, 17, 20, 18, 19), heap.getArray());
 
-        a = heap.exractMin();
+        a = heap.extractMin();
         assertEquals(15, a);
-        assertEquals(Arrays.asList(16, 18, 17, 19, 20), heap.getArray());
+        assertEquals(Arrays.asList(16, 18, 17, 20, 19), heap.getArray());
 
-        a = heap.exractMin();
+        a = heap.extractMin();
         assertEquals(16, a);
-        assertEquals(Arrays.asList(17, 18, 20, 19), heap.getArray());
+        assertEquals(Arrays.asList(17, 18, 19, 20), heap.getArray());
 
-        a = heap.exractMin();
+        a = heap.extractMin();
         assertEquals(17, a);
-        assertEquals(Arrays.asList(18, 19, 20), heap.getArray());
+        assertEquals(Arrays.asList(18, 20, 19), heap.getArray());
 
-        a = heap.exractMin();
+        a = heap.extractMin();
         assertEquals(18, a);
         assertEquals(Arrays.asList(19, 20), heap.getArray());
 
-        a = heap.exractMin();
+        a = heap.extractMin();
         assertEquals(19, a);
         assertEquals(Collections.singletonList(20), heap.getArray());
 
-        a = heap.exractMin();
+        a = heap.extractMin();
         assertEquals(20, a);
         assertEquals(Collections.emptyList(), heap.getArray());
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void extractMinWithEmpty() {
+    public void extractMinWithEmptyTest() {
         heap = new MinimumBinaryHeap();
-        heap.exractMin();
+        assertTrue(heap.isEmpty());
+        heap.extractMin();
     }
 }
 
